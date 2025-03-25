3b8bf126e45352cdb1e7444c860402b2e2420c96
Punam Thapa <punamthapamagar24@gmail.com>
2025-03-25 18:35:13 +0545

    numbering using js

--- Numbers.js
+++ Numbers.js
@@ -12,3 +12,19 @@ for (let i = 1; i <= upto; i++) {
 }
 numbersDiv.innerHTML = numbersHtml;
 }
+
+
+// let titleH1 = document.getElementById("title");
+// titleH1.style.color = "green";
+// titleH1.style.fontFamily = "Helvetica";
+
+// function printNumbers(){
+//    let upto = document.getElementById("UpperRange").value
+//    titleH1.innerText = "Numbers from 1 to 500" ;
+//    let numbersDiv = document.getElementById("numbers");
+// let numbersHtml = "";
+// for (let i = 1; i <= 500; i++) {
+//     numbersHtml = numbersHtml + "<h2  class='badge text-bg-danger'>"+i+"</h2>";
+// }
+// numbersDiv.innerHTML = numbersHtml;
+
